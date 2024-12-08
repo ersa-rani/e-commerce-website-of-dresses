53c41aabd56ed68e765bfc6d7e4ed99800c0a675
ersa rani <161242905+ersa-rani@users.noreply.github.com>
2024-12-09 01:08:40 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,46 @@
-import Image from "next/image";
 
-export default function Home() {
+export default function Home()  {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+    <div className="w-[1440px] flex bg-black h-[38px] pt-[9px] pr-[100px] pb-[9px] pl-[544px] gap-4 items-center">
+      <p className="font-satoshi text-[14px] font-normal leading-[18.9px] text-left text-white underline-offset-auto decoration-slice">
+        Sign up and get 20% off to your first order.
+        <span className="font-satoshi text-[14px] font-medium underline decoration-solid underline-offset-auto">
+          Sign Up Now
+        </span>
+      </p>
     </div>
+
+<div   className="bg-home">
+<img src="/vector.png" alt="vector" className="vector1"/>
+<img src="/vector.png" alt="vector" className="vector2"/>
+<p className='index1'>
+  FIND CLOTHES THAT MATCHES YOUR STYLE</p>
+  <p className='index2'>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and
+     cater to your sense of style.</p>
+     <button className="shop-Button">Shop Now</button>
+</div><div className="brand-list">
+<li>VERSACE</li><li>ZARA</li><li>GUCCI</li><li>PRADA</li><li>Calvin Klein</li></div>
+<Arrivals/>
+</div>
   );
+};
+
+export function Arrivals(){
+  return(
+    <div>
+      <h1 className="new-arrivals">NEW ARRIVALS</h1>
+      <div>
+        <img src="/frame1.png" alt="product"className="product1"/><p className="product-1-price">$120</p>
+        <img src="/frame1Detail.png" alt="Details"className="product1Detail"/>
+        <img src="/frame2.png" alt="product"className="product2"/>
+        <img src="/frame2Detail.png" alt="Details"className="product2Detail"/><p className="product-2-price">$240 <s>$260</s></p>
+        <img src="/frame3.png" alt="product"className="product3"/>
+        <img src="/frame1Detail.png" alt="Details"className="product3Detail"/><p className="product-3-price">$180</p>
+        <img src="/frame4.png" alt="product"className="product4"/>
+        <img src="/frame1Detail.png" alt="Details"className="product4Detail"/><p className="product-4-price">$130<s>$160</s></p>
+      </div><button className="view-button">View All</button>
+  </div>
+    
+  )
 }
