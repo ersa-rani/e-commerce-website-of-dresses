65c572134d04c13fc80f27e610d1fda0cc87d107
ersa rani <161242905+ersa-rani@users.noreply.github.com>
2024-12-17 19:26:02 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-import { GrUserManager } from "react-icons/gr";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
